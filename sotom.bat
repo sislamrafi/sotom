b11d3e4ccb7559e3792ae7a2d910f87452ed33e0
sislamrafi <sislamrafi333@gmail.com>
2022-07-21 18:47:14 +0600

    reduce python libs

--- sotom.bat
+++ sotom.bat
@@ -4,4 +4,4 @@
 @REM echo *** SOTOM FRAMEWORK COMMAND LINE ***
 @REM echo -----------------------------------------
 @REM REM: print new line
-C:\Users\sislamrafi\.virtualenvs\test-aSi4skF0\Scripts\python.exe %~dp0/sotom.py %*
+F:\SoftwareWorks\DJango\PYENV\test30141\Scripts\python.exe %~dp0/sotom.py %*
