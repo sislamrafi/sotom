11028217b18f2dd5a21c8bf7378a0fdf3a0103d9
sislamrafi <sislamrafi333@gmail.com>
2022-07-21 18:51:27 +0600

    use global python env

--- sotom.bat
+++ sotom.bat
@@ -4,4 +4,4 @@
 @REM echo *** SOTOM FRAMEWORK COMMAND LINE ***
 @REM echo -----------------------------------------
 @REM REM: print new line
-F:\SoftwareWorks\DJango\PYENV\test30141\Scripts\python.exe %~dp0/sotom.py %*
+python.exe %~dp0/sotom.py %*
